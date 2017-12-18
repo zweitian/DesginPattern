7e672a12082c6aa728628a05b4546b486a5802f8
ztian <zhu.weitian@foxmail.com>
2017-12-18 22:03:29 +0800

    facade pattern

---
+++ src/FacadePattern_07/Stock1.java
@@ -0,0 +1,15 @@
+package FacadePattern_07;
+
+/**
+ * @Author:ztian
+ * @Description:
+ * @CreateTime: 2017/12/18  21:53
+ */
+public class Stock1 {
+    public void sell(){
+        System.out.println("卖出股票1");
+    }
+    public void buy(){
+        System.out.println("买入股票1");
+    }
+}
